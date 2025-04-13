7be82ce48e61bcd03df09e92996016031581ac07
Yuri Kozhanov <ykozhanov97@gmail.com>
2025-04-13 16:42:23 +0500

    Rename tags

--- src/utils/add_routers_util.py
+++ src/utils/add_routers_util.py
@@ -3,7 +3,7 @@ from src.routers import table_router, reservation_router
 
 
 def add_routers(app: FastAPI) -> None:
-    app.include_router(table_router, prefix="/tables", tags=["tables"])
+    app.include_router(table_router, prefix="/tables", tags=["Столики"])
     app.include_router(
-        reservation_router, prefix="/reservations", tags=["reservations"]
+        reservation_router, prefix="/reservations", tags=["Брони"]
     )
