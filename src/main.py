5ed6f9d936f3a3811a142f8c53154e15e9974447
Yuri Kozhanov <ykozhanov97@gmail.com>
2025-04-13 16:48:08 +0500

    Update swagger

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from src.utils import add_routers, add_exception_handlers
 
-app = FastAPI()
+app = FastAPI(
+    title="Booking",
+    description="Микросервис для бронирования столиков в ресторане с поддержкой временных слотов и проверкой конфликтов",
+    version="0.0.1",
+)
 
 add_routers(app)
 add_exception_handlers(app)
